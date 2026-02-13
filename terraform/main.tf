68c79bcd27fe2b2ab1c50d6605b19b503d3cccce
simon <simon_tech_84@outlook.com>
2026-02-13 11:31:59 +0000

    Updated secuirty groups with restricted public IP ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["86.138.112.208/32"]
   }
   # Allow HTTP
   ingress {
@@ -47,7 +47,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["86.138.112.208/32"]
   }
   # Allow all outbound traffic
   egress {
